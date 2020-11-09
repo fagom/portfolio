71d10bafe6bf2914ba6bb20eb5c8a0fd049b7ba4
fagom174 <pradhan.subhasis174@gmail.com>
2020-11-09 23:46:13 +0530

    Express route to Client for static files

--- dist/index.js
+++ dist/index.js
@@ -35,4 +35,13 @@ app.use(getTimelineDetailsRoute);
 app.use(getSkillDetailsRoute);
 app.use(getProjectDetailsRoute);
 const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+    //Express will server up production assets like main.js or main.css file
+    app.use(express_1.default.static("client/build"));
+    //Express will serve up index.html if it doesnt recognise the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 app.listen(PORT, () => console.log(`Server API App listening on Port ${PORT}`));
--- index.ts
+++ index.ts
@@ -36,4 +36,14 @@ app.use(getProjectDetailsRoute);
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  //Express will server up production assets like main.js or main.css file
+  app.use(express.static("client/build"));
+  //Express will serve up index.html if it doesnt recognise the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`Server API App listening on Port ${PORT}`));
